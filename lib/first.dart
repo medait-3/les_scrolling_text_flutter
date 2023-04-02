bc9884fea0b38cecc068e7739fb24e5d64004842
meait-3 <tchikh3@@gmail.com>
2023-04-02 17:48:10 +0000

    add a rotation screen

--- lib/first.dart
+++ lib/first.dart
@@ -1,10 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:text_scroll/text_scroll.dart';
 
-class WelcomePage extends StatelessWidget {
+class WelcomePage extends StatefulWidget {
   String name, email;
   WelcomePage({Key? key, required this.name, required this.email})
       : super(key: key);
+
+  @override
+  State<WelcomePage> createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
+  // Step 2
+  void initState() {
+    super.initState();
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeRight,
+      DeviceOrientation.landscapeLeft,
+    ]);
+  }
+
+// Step 3
+  @override
+  dispose() {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeRight,
+      DeviceOrientation.landscapeLeft,
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,14 +44,14 @@ class WelcomePage extends StatelessWidget {
               Padding(
                 padding: const EdgeInsets.all(82.0),
                 child: TextScroll(
-                  '  $name  ******************',
+                  '  ${widget.name}  ******************',
                   intervalSpaces: 10,
                   velocity: Velocity(pixelsPerSecond: Offset(50, 0)),
                   style: TextStyle(color: Colors.black, fontSize: 22),
                 ),
               ),
-              Text('Name: $name'),
-              Text('Email: $email'),
+              Text('Name: ${widget.name}'),
+              Text('Email: ${widget.email}'),
             ],
           ),
         ),
